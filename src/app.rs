f6dcac6117f3829518e239cbe42dbe1ee3da7241
Markus Ineichen <kusi.ineichen@gmail.com>
2025-09-17 15:59:42 +0200

    Bring timeout into stream

--- src/app.rs
+++ src/app.rs
@@ -18,14 +18,16 @@ async fn call_model(
     let mut first: String = iter.next().expect("Has at least one word").into();
     first.reserve(reply.len() - first.len());
     set_messages.update(|msgs| msgs.push(("AI".to_string(), first)));
-    let mut stream = futures_util::stream::iter(iter);
+    let mut stream = std::pin::pin!(futures_util::stream::iter(iter).then(|w| async move {
+        gloo_timers::future::sleep(std::time::Duration::from_millis(100)).await;
+        w
+    }));
     while let Some(word) = stream.next().await {
         set_messages.update(|x| {
             let msg = &mut x.last_mut().unwrap().1;
             msg.push(' ');
             *msg += word;
         });
-        gloo_timers::future::sleep(std::time::Duration::from_millis(100)).await;
     }
 }
 
